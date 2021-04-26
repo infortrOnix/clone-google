ecfc17a137823af58ad0b3029eabadc6ab855062
Valeria Occhiuzzi <valerisvso@gmail.com>
2021-04-26 00:25:08 -0300

    Delete keys.js

--- keys.js
+++
@@ -1,6 +0,0 @@
-// API key - https://developers.google.com/custom-search/v1/using_rest
-export const API_KEY = 'AIzaSyCpFjgH378Vl7DYocKtycipc_byGoASzvo'
-
-// Context key - https://cse.google.com/cse/create/new
-// es el contexto del searching
-export const CONTEXT_KEY = 'e6e31fec6743e9b05'
